fb196e7f4ae1845a57793e5b4532d3a9c6e579b1
Matthew Dey <matthewdey9@gmail.com>
2019-06-27 08:48:43 -0400

    basic skeleton app created

---
+++ src/main/java/cs338/gui/MainFrame.java
@@ -0,0 +1,34 @@
+/*
+ * MainFrame.java
+ * cs338-gui
+ * Drexel University - CS338
+ * 
+ * Author: Matthew Dey
+ * Date Created: 
+ * 
+ * MainFrane.java is the main GUI frame for the application.
+ * 
+ */
+
+package cs338.gui;
+
+import javax.swing.*;
+
+ public class MainFrame {
+
+    JFrame frame;
+    JButton button;
+
+    MainFrame(int width, int length, String framename, String buttonname) {
+        this.frame = new JFrame(framename);
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setSize(width,length);
+        this.button = new JButton(buttonname);
+        this.frame.getContentPane().add(button);
+    }
+
+    public void run() {
+        this.frame.setVisible(true);
+        return;
+    }
+}
